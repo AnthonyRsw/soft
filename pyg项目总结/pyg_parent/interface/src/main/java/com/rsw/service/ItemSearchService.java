8149823ab9bab2ae0db7beeaf7bdd29b6b9afe8e
rsw <2217291861@qq.com>
2019-08-30 11:08:57 +0800

    2

--- pyg项目总结/pyg_parent/interface/src/main/java/com/rsw/service/ItemSearchService.java
+++
@@ -1,7 +0,0 @@
-package com.rsw.service;
-
-import java.util.Map;
-
-public interface ItemSearchService {
-    Map<String,Object> search(Map searchMap);
-}
--- pyg项目总结/pyg_parent/interface/src/main/java/com/rsw/service/SolrManagerService.java
+++
@@ -1,8 +0,0 @@
-package com.rsw.service;
-
-public interface SolrManagerService {
-
-    void saveItemByGoodId(Long id);
-
-    void deleteItemFromSolr(Long id);
-}
